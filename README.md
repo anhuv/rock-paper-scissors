bcbee3be6feba2d9939119b48d30ea8ea1fc011b
ungvietanh20172394 <ungvietanh120799@gmail.com>
2021-04-25 10:50:30 +0700

    add comment

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # rock-paper-scissors
 
-![visitors](https://visitor-badge.glitch.me/badge?page_id=SouravJohar.visitor-badge)
-
-https://youtu.be/0uSA3xyXlwM 
 
 An AI to play the Rock Paper Scissors game
 
@@ -23,26 +20,26 @@ $ cd rock-paper-scissors
 ```sh
 $ pip install -r requirements.txt
 ``` 
-cai ban moi nhat nhe, khong cai theo requirements.txt
+cài bản mới nhất, không cài theo requirements.txt
 
 3. Gather Images for each gesture (rock, paper and scissors and None):
 In this example, we gather 200 images for the "rock" gesture
 ```sh
-$ python3 gather_images.py rock 200
+$ python gather_images.py rock 200
 ```
 
 4. Train the model
 ```sh
-$ python3 train.py
+$ python train.py
 ```
 
 5. Test the model on some images
 ```sh
-$ python3 test.py <path_to_test_image>
+$ python test.py <path_to_test_image>
 ```
-khong can lam buoc 5
+không cần làm bước 5
 
 6. Play the game with your computer!
 ```sh
-$ python3 play.py
+$ python play.py
 ```
--- train.py
+++ train.py
@@ -27,11 +27,20 @@ def mapper(val):
 def get_model():
     model = Sequential([
         VGG16(weights='imagenet', include_top=False, input_shape=(227, 227, 3)),
+        # https://phamdinhkhanh.github.io/2020/05/31/CNNHistory.html xem về vgg16, ở đây chỉ lấy một phần vgg16, kĩ thuật này gọi là tranfer learning
+        # đầu ra thu được ma trận dạng 7x7x512
         Dropout(0.5),
         Convolution2D(NUM_CLASSES, (1, 1), padding='valid'),
+        # NUM_CLASSES = 4, kích thước cửa sổ là 1x1 (kích thước ma trận không thay đổi), vì vậy vẫn là ma trận 7x7, nhưng khi qua layer này thì thành 7x7x4
+        # đầu ra thu được ma trận dạng 7x7x4
         Activation('relu'),
         GlobalAveragePooling2D(),
+        # lấy trung bình của 4 ma trận 7x7, tưởng tượng như 7x7 thành 1x1 do nó lấy trung bình tất cả các phần từ trong ma trận 7x7
+        # như vậy từ 7x7x4 thành 1x1x4 ( hoặc kích thước từ 7x7x4 thành 4)
         Activation('softmax')
+        # lựa chọn softmax thì đầu ra sẽ có kết quả 0 hoặc 1
+        # ví dụ kết quả là [1 0 0 0] thì là rock
+        # tương tự có các kết quả cho "paper", "scissors", "none"
     ])
     return model
 
